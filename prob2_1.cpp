daa5ff998fe377ac7f6d6fc93c39348c7d1c8a70
Octavian Jidovu <75202003+SpaceWanderer42@users.noreply.github.com>
2022-04-27 15:27:13 +0300

    Create prob2_1.cpp
    Se dau de la tastatură un număr necunoscut de mare de perchi de două sau trei numere fracționare reprezentabile cu dublă precizie (până la EOF). Aceste perechi reprezintă coordonatele X și Y sau X, Y și Z, ale unor puncte într-un spațiu de coordonate cartezian tridimensional. Punctele cărora le lipsește coordonate Z, aceasta se consideră implicit 0. Transformați valorile într-un sistem de coordonate sferic și afișați pentru fiecare punct valorile distanței până la origine r, precum și unghiurile φ și θ.
    
    Cerințe pentru punctaj maxim
    Să se creeze o clasă cu numele Point cu următoarele specificații:
    - clasa trebuie să conțină 3 atribute private numite x, y și z. Tipul atributelor trebuie dedus de voi;
    - clasa trebuie să conțină un constructor public cu 0 parametrii care să inițializeze coordonatele punctelor cu valoarea 0;
    - clasa trebuie să conțină un constructor public cu 2 parametrii care să inițializeze coordonatele x și y cu valorile specificate și coordonata z cu valoarea 0;
    - clasa trebuie să conțină un constructor public cu 3 parametrii care să inițializeze coordonatele punctelor cu valorile specificate;
    - clasa trebuie să conțină 3 metode publice fără niciun parametru numite getX, getY și getZ. Acestea trebuie să întoarcă valorile coordonatelor punctelor. Tipul de dată returnat al metodelor trebuie să îl deduceți voi;
    - clasa trebuie să conțină 3 metode publice fără niciun parametru numite getRadius, getPhi și getTheta. Acestea trebuie să întoarcă raza r, precum și cele 2 unghuri φ și θ. Tipul de dată returnat al metodelor trebuie să îl deduceți voi.
    
    Date de intrare 
    Un număr necunoscut de perechi de numere fracționare reprezentabile în dublă precizie, numerele dintr-o pereche separate prin punct-și-virgulă, perechile separate prin unul sau mai multe spații.
    
    Date de ieșire 
    Pe fiecare linie se vor afișa coordonatele polare ale fiecărui punct de la intrare, în ordinea INVERSĂ a apariției, în formatul (r=<val>; phi=<val>), SAU (r=<val>; phi=<val>, th=<val>),  valorile afișate cu trei zecimale, iar unghiurile exprimat în grade. Valoarea unghiurilor trebuie să fie pozitivă, în intervalul [0; 360) de grade. Dacă coordonata Z lipsește, unghiul θ nu se va afișa.
    
    Exemplu
    Intrare
    
    1.1;0 1;1;1
    0;2;0
    Ieșire
    
    (r=2.000; phi=90.000; th=90.000)
    (r=1.732; phi=45.000; th=45.000)
    (r=1.100; phi=0.000)
    
    Explicație
    
    
    Pentru tranformarea în coordonate sferice, citiți acest articol. În C++ există funcția atan2 și constanta M_PI.

---
+++ prob2_1.cpp
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <iomanip>
+#include <climits>
+
+using namespace std;
+
+class Point
+{
+    double x, y, z;
+    bool containsAzimuth = true;
+
+public:
+    Point()
+    {
+        x = y = z = 0.0;
+    }
+    Point(double x, double y)
+    {
+        this->x = x;
+        this->y = y;
+        z = 0.0;
+        containsAzimuth = false;
+    }
+    Point(double x, double y, double z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+    double radToDeg(double val)
+    {
+       val=val*180/M_PI;
+        
+        if(val < 0)
+        {
+            val = val + 360;
+            return val;
+            
+        }
+        else return val ;
+        
+    }
+
+    double getX()
+    {
+        return x;
+    }
+
+    double getY()
+    {
+        return y;
+    }
+
+    double getZ()
+    {
+        return z;
+    }
+
+    double getRadius()
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    double getPhi()
+    {
+        return radToDeg(atan2(y, x));
+    }
+
+    double getTheta()
+    {
+                if (containsAzimuth)
+            return radToDeg((atan2(sqrt(x * x + y * y), z)));
+        return 0;
+
+    }
+};
+
+int main()
+{
+    vector<Point> v;
+    double x, y, z = INT_MIN, prev_z = INT_MIN;
+    int i=scanf("%lf;%lf;%lf;",&x,&y,&z);
+    while(i!=EOF){
+         if(i==2){
+         v.push_back(Point(x,y));
+        //printf("%.0lf:%.0lf",x,y);
+         }
+         if(i==3){
+         v.push_back(Point(x,y,z));
+         //cout<<x<<":"<<y<<":"<<z<<endl;
+         }
+          i=scanf("%lf;%lf;%lf;",&x,&y,&z);
+     }
+   
+
+    for (auto it = v.rbegin(); it != v.rend(); ++it)
+    {
+        std::cout << std::fixed;
+        std::cout << std::setprecision(3);
+        cout << "(r=" << it->getRadius() << "; phi=" << it->getPhi() << "";
+        double th = it->getTheta();
+        if (th!=0) cout << "; th=" << th << ")";
+        else cout << ")";
+        cout << endl;
+    }
+    //cin.ignore();
+    //cin.get();
+    
+    return 0;
+}
